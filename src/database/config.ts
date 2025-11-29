ec01313a393ca58027c0c594376429cb0f18232b
angel cab <angelcab7u7@gmail.com>
2025-11-29 16:21:55 -0600

    cambios generales

--- src/database/config.ts
+++ src/database/config.ts
@@ -2,6 +2,6 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const sqlite: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: '../../db.sqlite',
+  database: `${process.cwd()}/db.sqlite`,
   entities: [`${__dirname}/entities/**/*{.ts,.js}`],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,8 @@
-// api/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { INestApplication } from '@nestjs/common';
-import { VercelRequest, VercelResponse } from '@vercel/node';
 
-let cachedApp: INestApplication;
-
-async function bootstrap() {
-  if (!cachedApp) {
-    const app = await NestFactory.create(AppModule, {
-      logger: ['error', 'warn', 'log'],
-    });
-    await app.init();
-    cachedApp = app;
-  }
-  return cachedApp;
+async function application(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-  const app = await bootstrap();
-  // Use the underlying platform's handler
-  const expressApp = app.getHttpAdapter().getInstance();
-  expressApp(req, res);
-}
+application().catch(() => console.log('Error en la api'));
