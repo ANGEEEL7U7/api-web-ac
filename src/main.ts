321853ad1f21f3ce18c2abef000ddacd9584a583
angel cab <angelcab7u7@gmail.com>
2025-11-28 21:24:50 -0600

    cambios generales

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { Server } from 'http';
-let server: Server;
+
 export async function bootstrap() {
-  if (server) return server;
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.init();
-  server = app.getHttpServer();
+  await app.listen(3000);
 }
 bootstrap().catch(() => console.log('Error en la api'));
