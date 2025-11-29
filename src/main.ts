5508f81dddc43bd91fe6fc3b87d062ac83dce143
angel cab <angelcab7u7@gmail.com>
2025-11-28 21:28:53 -0600

    cambios generales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+// api/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { INestApplication } from '@nestjs/common';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+let cachedApp: INestApplication;
+
+async function bootstrap() {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, {
+      logger: ['error', 'warn', 'log'],
+    });
+    await app.init();
+    cachedApp = app;
+  }
+  return cachedApp;
 }
-bootstrap().catch(() => console.log('Error en la api'));
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  const app = await bootstrap();
+  // Use the underlying platform's handler
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp(req, res);
+}
