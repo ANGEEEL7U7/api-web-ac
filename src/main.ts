7a127de669c26df684634282f31441a253ca680d
angel cab <angelcab7u7@gmail.com>
2025-11-28 20:50:55 -0600

    cambios app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import dotenv from 'dotenv';
 
 export async function bootstrap() {
-  const env = dotenv.config({ path: `${process.cwd()}/.env` });
-  if (env.error !== undefined)
-    throw new Error(`Error en el env ${env.error.message}`);
-
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap().catch(() => console.log('Error en la api'));
