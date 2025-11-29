74dc1c1de4bf872a8dadc266747dc0fc843a53c1
angel cab <angelcab7u7@gmail.com>
2025-11-28 21:40:24 -0600

    cambios generales

--- src/database/config.ts
+++ src/database/config.ts
@@ -2,6 +2,6 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const sqlite: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: `${process.cwd()}/db.sqlite`,
+  database: '../../db.sqlite',
   entities: [`${__dirname}/entities/**/*{.ts,.js}`],
 };
